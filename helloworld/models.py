3934a92190f94358edc4110d203633e5080d7243
Muhammad Shoaib <muhammad.shoaib@arbisoft.com>
2015-11-02 00:28:48 +0500

    - created the new model UserInfo to save the user information in the database   - register the model in the django admin panel   fixed the bug in the unicode string

--- helloworld/models.py
+++ helloworld/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class UserInfo(models.Model):
+    """
+    Django model to save the user name in the database
+    with the creation date/time
+    """
+
+    name = models.CharField(max_length=255, null=True)
+    created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __unicode__(self):
+        """
+        How to serialize myself as a string
+        """
+
+        return "{name}: {created_date}".format(
+            name=self.name,
+            created_date=self.created.strftime('%l:%M%p %Z on %b %d, %Y')
+        )
